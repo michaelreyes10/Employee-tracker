cc3f87e507949a969f497c4f1e373c9cb304b5e3
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-09-30 12:53:06 -0400

    corrected db.sql

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE employee_tracker;
-DROP DATABASE IF EXISTS employee_tracker;
-USE employee_tracker;
+DROP DATABASE IF EXISTS employees_tracker;
+CREATE DATABASE employees_tracker;
+USE employees_tracker;
